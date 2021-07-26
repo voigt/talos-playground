1222953fd1a00dc024ea74df6c86d7fac6dc86c5
Christoph Voigt <voigt.christoph@gmail.com>
2021-07-26 21:09:09 +0200

    comment out

--- terraform-digitalocean-talos/versions.tf
+++ terraform-digitalocean-talos/versions.tf
@@ -4,23 +4,23 @@ terraform {
   required_providers {
     external = {
       source  = "hashicorp/external"
-      version = ">= 1.2.0"
+      version = ">= 2.1.0"
     }
     local = {
       source  = "hashicorp/local"
-      version = ">= 2.0.0"
+      version = ">= 2.1.0"
     }
     null = {
       source  = "hashicorp/null"
-      version = ">= 3.0.0"
+      version = ">= 3.1.0"
     }
     time = {
       source  = "hashicorp/time"
-      version = ">= 0.6.0"
+      version = ">= 0.7.2"
     }
     random = {
       source  = "hashicorp/random"
-      version = ">= 0.6.0"
+      version = ">= 3.1.0"
     }
   }
 }
