b4416339d7a40842b435a9be13da7972db378305
Christoph Voigt <voigt.christoph@gmail.com>
2021-07-26 20:57:59 +0200

    add pulumi

--- digitalocean/pulumi/go.mod
+++ digitalocean/pulumi/go.mod
@@ -3,6 +3,7 @@ module talos
 go 1.14
 
 require (
+	github.com/pulumi/pulumi-cloudflare/sdk/v3 v3.3.0
 	github.com/pulumi/pulumi-digitalocean/sdk/v4 v4.4.1
 	github.com/pulumi/pulumi/sdk/v3 v3.6.1
 )
--- digitalocean/pulumi/main.go
+++ digitalocean/pulumi/main.go
@@ -1,22 +1,43 @@
 package main
 
 import (
+	"fmt"
+
+	"github.com/pulumi/pulumi-cloudflare/sdk/v3/go/cloudflare"
 	"github.com/pulumi/pulumi-digitalocean/sdk/v4/go/digitalocean"
 	"github.com/pulumi/pulumi/sdk/v3/go/pulumi"
 )
 
 func main() {
 	pulumi.Run(func(ctx *pulumi.Context) error {
-		// Create a DigitalOcean resource (Domain)
-		domain, err := digitalocean.NewDomain(ctx, "my-domain", &digitalocean.DomainArgs{
-			Name: pulumi.String("my-domain.io"),
-		})
-		if err != nil {
-			return err
+
+		for i := 0; i < 3; i++ {
+			dropletName := "control-plane-" + fmt.Sprint(i)
+			droplet, err := digitalocean.NewDroplet(ctx, dropletName, &digitalocean.DropletArgs{
+				Image:   pulumi.String("87607619"),
+				Region:  pulumi.String("fra1"),
+				Size:    pulumi.String("s-2vcpu-4gb"),
+				SshKeys: pulumi.ToStringArray([]string{"30756593"}),
+			})
+			if err != nil {
+				return err
+			}
+
+			ctx.Export(dropletName, droplet.Ipv4Address)
+
+			dnsName := "talosDNS-" + fmt.Sprint(i)
+			_, err = cloudflare.NewRecord(ctx, dnsName, &cloudflare.RecordArgs{
+				Name:   pulumi.String("talos"),
+				ZoneId: pulumi.String("5135e0adad7dabaa3699abac5bba8559"),
+				Type:   pulumi.String("A"),
+				Value:  pulumi.StringOutput(droplet.Ipv4Address),
+				Ttl:    pulumi.Int(3600),
+			})
+			if err != nil {
+				return err
+			}
 		}
 
-		// Export the name of the domain
-		ctx.Export("domainName", domain.Name)
 		return nil
 	})
 }
