729e7372fde5027e9942347cf8f20406f176599b
Christoph Voigt <voigt.christoph@gmail.com>
2021-08-01 23:24:39 +0200

    add worker_config local_file

--- terraform-digitalocean-talos/main.tf
+++ terraform-digitalocean-talos/main.tf
@@ -147,6 +147,40 @@ resource "local_file" "controlplane_config" {
 }
 
 # Generate the Talos worker.yaml files
+resource "local_file" "worker_config" {
+
+  content = templatefile("${path.module}/talosworker.tpl", {
+    tf_talos_token       = format("%s.%s", substr(random_string.random_token[0].result, 7, 6), substr(random_string.random_token[0].result, 17, 16))
+    tf_type              = "worker"
+    tf_talos_ca_crt      = data.external.talos_certificates.result.talos_crt
+    tf_talos_ca_key      = data.external.talos_certificates.result.talos_key
+    tf_host_arch         = data.external.talos_certificates.result.host_arch
+    tf_kube_version      = var.kube_version
+    tf_talos_version     = var.talos_version
+    # tf_cluster_endpoint  = format("%s.%s", var.kube_cluster_name, var.dns_domain)
+    tf_cluster_endpoint  = module.talos_loadbalancer.loadbalancer_ip
+    tf_cluster_name      = var.kube_cluster_name
+    tf_kube_dns_domain   = var.kube_dns_domain
+    tf_kube_token        = format("%s.%s", substr(random_string.random_token[1].result, 5, 6), substr(random_string.random_token[1].result, 15, 16))
+    tf_kube_enc_key      = base64encode(random_string.random_key[0].result)
+    tf_kube_ca_crt       = data.external.talos_certificates.result.kube_crt
+    tf_kube_ca_key       = data.external.talos_certificates.result.kube_key
+    tf_etcd_ca_crt       = data.external.talos_certificates.result.etcd_crt
+    tf_etcd_ca_key       = data.external.talos_certificates.result.etcd_key
+    tf_aggregator_ca_crt = data.external.talos_certificates.result.aggregator_crt
+    tf_aggregator_ca_key = data.external.talos_certificates.result.aggregator_key
+    tf_sa_ca_key         = data.external.talos_certificates.result.sa_key
+    tf_allow_scheduling  = var.controlplane_scheduling
+  })
+  filename   = "${abspath(var.conf_dir)}/worker.yaml"
+
+  depends_on = [
+    module.talos_loadbalancer,
+    data.external.talos_certificates,
+    random_string.random_token,
+    random_string.random_key
+  ]
+}
 
 # Provide the Talos configuration yaml files, to the newly booted up VirtualBox VMs
 resource "null_resource" "os_install" {
